d28f74b9c6539a0890a2572df3ea1a36c55f2852
Martin Walther <it@martin-walther.ch>
2019-01-13 14:20:31 +0100

    Update PsNetTools.ps1

--- PsNetTools.ps1
+++ PsNetTools.ps1
@@ -4,6 +4,12 @@
     [PsNetTools]::dig('') 
     [PsNetTools]::dig('sbb.ch')
     [PsNetTools]::dig('sbb.chr')
+
+    tping - tcp port scanner
+    [PsNetTools]::tping() 
+    [PsNetTools]::tping('sbb.ch') 
+    [PsNetTools]::tping('sbb.ch', 80) 
+    [PsNetTools]::tping('sbb.ch', 80, 100)
 #>
 Class PsNetTools {
 
@@ -60,7 +66,55 @@ Class PsNetTools {
         return $resultset
     }
 
+    [void]static tping() {
+        $function  = 'tping()'
+        Write-Warning "$($function): No Target specified!"
+    }
 
+    [void]static tping([String] $TargetName, [int] $TcpPort) {
+        $function  = 'tping()'
+        Write-Warning "$($function): No TcpPort or Timeout specified!"
+    }
+
+    [object]static tping([String] $TargetName, [int] $TcpPort, [int] $Timeout) {
+
+        $function  = 'tping()'
+        $resultset = @()
+
+        if(([String]::IsNullOrEmpty($TargetName))){
+            Write-Warning "$($function): Empty TargetName specified!"
+        }
+        else{
+            $tcpclient    = $null
+            $connect      = $null
+            $patience     = $null
+            $tcpsucceeded = $null
+
+            try {
+                $tcpclient = New-Object System.Net.Sockets.TcpClient
+                $connect   = $TcpClient.BeginConnect($TargetName,$TcpPort,$null,$null)
+                $patience  = $connect.AsyncWaitHandle.WaitOne($Timeout,$false) 
+        
+                $tcpsucceeded = $tcpclient.Connected
+                $tcpclient.EndConnect($connect)
+                $tcpclient.Close()
+                $tcpclient.Dispose()
+        
+                $obj = [PSCustomObject]@{
+                    TargetName    = $TargetName
+                    TcpPort       = $TcpPort
+                    TcpSucceeded  = $tcpsucceeded
+                    MaxTimeout    = "$($Timeout)ms"
+                }
+                $resultset += $obj
+        
+            } catch {
+                Write-Warning "$($function): Could not connect to $TargetName!"
+                $error.Clear()
+            }                
+        }    
+        return $resultset    
+    }
 
     #endregion
 }
