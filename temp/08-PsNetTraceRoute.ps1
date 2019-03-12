911a275e03d8545bbd3042ea7109aca363ee05cf
Walther Martin <Martin.Walther@swisscom.com>
2019-03-12 22:29:46 +0100

    Update Trace route

--- temp/08-PsNetTraceRoute.ps1
+++ temp/08-PsNetTraceRoute.ps1
@@ -1,18 +1,80 @@
-$destination = 'sbb.ch'
+function GetTraceRoute{
 
-$pingsender  = [System.Net.NetworkInformation.Ping]::new()
-$pingoptions.DontFragment = $true
+    param(
+        [string] $Destination,
+        [int] $maxTimeout = 1000,
+        [int] $maxHops    = 30,
+        [switch] $show    = $false
+    )
 
-[int] $timeout    = 10000
-[int] $maxTTL     = 30
+    if($show){
+        Write-Output "Trace route $Destination over $maxHops Hops"
+    }
 
-$dgram = new-object System.Text.ASCIIEncoding
-$byte  = $dgram.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    $resultset   = @()
 
-for($ttl = 1; $ttl -lt $maxTTL; $ttl ++){
-    $pingoptions = [System.Net.NetworkInformation.PingOptions]::new($ttl, $true)
+    $pingsender  = [System.Net.NetworkInformation.Ping]::new()
+    $datagram    = new-object System.Text.ASCIIEncoding
+    $buffersize  = $datagram.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
-    $reply = $pingsender.Send($destination, $timeout, $byte, $pingoptions)
+    for($ttl = 1; $ttl -le $maxHops; $ttl ++){
 
-    $reply 
+        $ExitFlag  = $false
+        $dnsreturn = $null
+        [DateTime] $start = Get-Date
+
+        try{
+            $pingoptions = [System.Net.NetworkInformation.PingOptions]::new($ttl, $true)        
+            $reply       = $pingsender.Send($destination, $maxTimeout, $buffersize, $pingoptions)
+        }
+        catch{
+            $error.Clear()
+        }
+        $time = $([math]::round(((New-TimeSpan $($start) $(get-date)).TotalMilliseconds),0))
+
+        try{
+            if(-not([String]::IsNullOrEmpty($reply.Address))){
+                $dnsreturn = [System.Net.Dns]::GetHostByAddress($reply.Address).HostName
+            }
+        }
+        catch{
+            $dnsreturn = 'Could not resolve'
+            $error.clear()
+        }
+
+        switch($reply.Status){
+            'TtlExpired' {
+                # TtlExpired means we've found an address, but there are more addresses
+                $IPAddress = $reply.Address
+                $Status    = 'Running, there are more addresses'
+            }
+            'TimedOut'   {
+                # TimedOut means this ttl is no good, we should continue searching
+                $IPAddress = $reply.Address
+                $Status    = 'Request timed out, continue searching'
+            }
+            'Success'    {
+                # Success means the tracert has completed
+                $IPAddress = $reply.Address
+                $Status    = 'Success, trace route has completed'
+                $ExitFlag  = $true
+            }
+        }
+
+        $obj = [PSCustomObject]@{
+            TTL         = $ttl
+            TimeMs      = $time
+            Destination = $destination
+            Hostname    = $dnsreturn
+            IPAddress   = $IPAddress
+            Status      = $Status
+        }
+        $resultset += $obj
+
+        if($show){$obj}
+        if($ExitFlag){break}
+    }
+    if($show -eq $false){
+        return $resultset
+    }
 }
