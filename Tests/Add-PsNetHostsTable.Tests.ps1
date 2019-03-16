2da7f71ed81bb97642f221dba3787b35126f61c7
Walther Martin <Martin.Walther@swisscom.com>
2019-03-16 13:54:49 +0100

    Change Pester Tests for Add-/Remove Hostsentry

--- Tests/Add-PsNetHostsTable.Tests.ps1
+++ Tests/Add-PsNetHostsTable.Tests.ps1
@@ -20,17 +20,28 @@ else{
 
 Describe "Testing Add-PsNetHostsEntry on $($CurrentOS) OS" {
     
+    if(($CurrentOS -eq 'Mac') -or ($CurrentOS -eq 'Linux')){
+        $current   = (id -u)
+        $IsAdmin   = ($current -eq 0)
+    }
+
+    # For Windows only
+    if($CurrentOS -eq 'Win'){
+        $current   = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        $IsAdmin   = $current.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+
     it "[NEG] [$($CurrentOS)] Testing Add-PsNetHostsEntry"{
         (Add-PsNetHostsEntry -path 'C:\Temp\hosts' -IPAddress '127.0.0.1' -Hostname tinu -FullyQualifiedName tinu.walther.ch).Succeeded | should BeFalse
     }
-    if($CurrentOS -eq 'Win'){
+    if($IsAdmin){
         it "[POS] [$($CurrentOS)] Testing Add-PsNetHostsEntry"{
             (Add-PsNetHostsEntry -IPAddress '127.0.0.1' -Hostname tinu -FullyQualifiedName tinu.walther.ch).Succeeded | should BeTrue
         }
     }
     else{
         it "[POS] [$($CurrentOS)] Testing Add-PsNetHostsEntry"{
-            (Add-PsNetHostsEntry -IPAddress '127.0.0.1' -Hostname tinu -FullyQualifiedName tinu.walther.ch).Succeeded | should BeTrue
+            (Add-PsNetHostsEntry -IPAddress '127.0.0.1' -Hostname tinu -FullyQualifiedName tinu.walther.ch).Succeeded | should BeFalse
         }
     }
 
--- Tests/Remove-PsNetHostsTable.Tests.ps1
+++ Tests/Remove-PsNetHostsTable.Tests.ps1
@@ -20,17 +20,29 @@ else{
 
 Describe "Testing Remove-PsNetHostsEntry on $($CurrentOS) OS" {
     
+    if(($CurrentOS -eq 'Mac') -or ($CurrentOS -eq 'Linux')){
+        $current   = (id -u)
+        $IsAdmin   = ($current -eq 0)
+    }
+
+    # For Windows only
+    if($CurrentOS -eq 'Win'){
+        $current   = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        $IsAdmin   = $current.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    }
+
     it "[NEG] [$($CurrentOS)] Testing Remove-PsNetHostsEntry"{
         (Remove-PsNetHostsEntry -path 'C:\Temp\hosts' -Hostsentry '127.0.0.1 tinu').Succeeded | should BeFalse
     }
-    if($CurrentOS -eq 'Win'){
+
+    if($IsAdmin){
         it "[POS] [$($CurrentOS)] Testing Remove-PsNetHostsEntry"{
             (Remove-PsNetHostsEntry -Hostsentry '127.0.0.1 tinu').Succeeded | should BeTrue
         }
     }
     else {
         it "[POS] [$($CurrentOS)] Testing Remove-PsNetHostsEntry"{
-            (Remove-PsNetHostsEntry -Hostsentry '127.0.0.1 tinu').Succeeded | should BeTrue
+            (Remove-PsNetHostsEntry -Hostsentry '127.0.0.1 tinu').Succeeded | should BeFalse
         }
     }
 
